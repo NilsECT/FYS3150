dd3d2ddcbc46f29da96f4ad20c22763b4af0c0ba
Tensorboy2 <root@LAPTOP-RSSIV8M1.localdomain>
2022-09-02 10:52:04 +0200

    Hola!

---
+++ Sigurd/Problem_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("Problem_2.txt") #numpy's txt-reading
+
+x,y = data[:,0], data[:,1] #putting x and y into arrays
+
+plt.plot(x,y,label="u(x)") # plotting y over x
+
+#Plotting stuff:
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.savefig("Problem_2_plot.pdf")
