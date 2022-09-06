3f5bf5ebfa995246a6741816187dd4310e9a2a99
Tensorboy2 <root@LAPTOP-RSSIV8M1.localdomain>
2022-09-06 16:13:51 +0200

    Problem_7

--- Project_1/Code/Problem_7.cpp
+++ Project_1/Code/Problem_7.cpp
@@ -2,48 +2,54 @@
 #include <armadillo>
 #include <iostream>
 #include <fstream>
-using namespace std;
 
-double u(double x){ //Function for u-values
-    //Since we only have 4-desimals in the values we use floats without loosing precision
-    return 1-(1-exp(-10))*x - exp(-10*x);
-}
 
 double f(double x){
     return 100*exp(-10*x);
 }
 
-int main (){
-    int N = 1;
-    int n = 1000;
-    double h = 1./n;
-    arma::vec x = arma::linspace(0,N,n);
-    arma::vec g = arma::vec(n);   
-    for     (int i=0; i<n; i++){
+int main (int argc, char* argv[]){
+    double N = 1;
+    int n = atoi(argv[1]);
+    double h = N/n;
+    arma::vec x = arma::linspace(0,N,n+1);
+    arma::vec g = arma::vec(n+1); 
+    arma::vec gt = arma::vec(n+1);  // g-tilde
+    arma::vec v = arma::vec(n+1); 
+    arma::vec vt = arma::vec(n+1); // v-tilde
+    
+    v(0) = 0;
+    v(n) = 0;
+
+    g(1) = f(x(1))*h*h + v(0);
+    g(n-1) = f(x(n-1))*h*h + v(n);
+    for     (int i = 2; i<n-1; i++){
         g(i) = f(x(i))*h*h;
     }
 
-    arma::vec v = arma::vec(n); 
     arma::vec a = arma::vec(n).fill(-1.); 
-    arma::vec b = arma::vec(n).fill(2.); 
+    arma::vec b = arma::vec(n).fill(2.); //b
+    arma::vec bt = arma::vec(n); //b-tilde
     arma::vec c = arma::vec(n).fill(-1.); 
 
 
     // Start measuring time
     clock_t t1 = clock();
+
+    gt(1) = g(1);
+    bt(1) = b(1);
+    for     (int i = 2; i<n-1; i++){
+        bt(i) = b(i) - (a(i)/bt(i-1))*c(i-1);
+        gt(i) = g(i) - (a(i)/bt(i-1))*gt(i-1);
+    }
     
-    for     (int i = 1; i<n; i++){
-        b(i) = b(i) - (a(i)/b(i-1))*c(i-1);
-        g(i) = g(i) - (a(i)/b(i-1))*g(i-1);
-
+    //std::cout << "Hola" << std::endl;
+    
+    v(n-1) = gt(n-1)/bt(n-1);
+    
+    for     (int i=n-2; i>0; i--){
+        v(i) = (gt(i) - c(i)*v(i+1))/bt(i);
     }
-
-    v(n-1) = g(n-1)/b(n-1);
-
-    for     (int i=n-2; i>=0; i--){
-        v(i) = (g(i) - c(i)*v(i+1))/b(i);
-    }
-    //std::cout << g << std::endl;
     //std::cout << v << std::endl;
 
 
@@ -52,17 +58,16 @@ int main (){
 
     // Calculate the elapsed time.
     double duration_seconds = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
-    std::cout << duration_seconds << std::endl;
+    //std::cout << duration_seconds << std::endl;
 
-    arma::mat A = arma::mat(n, 3); //Defines matrix for easyer writing to file
-    for  (int i = 0; i < n; i++){ //for-loop of every element n 
+    arma::mat A = arma::mat(n+1, 2); //Defines matrix for easyer writing to file
+    for  (int i = 0; i < n+1; i++){ //for-loop of every element n 
         A(i,0) = x(i); //Defines x as 1. column in the matrix
-        A(i,1) = u(x(i)); //Defines u valus as 2. column in the matrix
-        A(i,2) = v(i);
+        //A(i,1) = u(x(i)); //Defines u valus as 2. column in the matrix
+        A(i,1) = v(i);
     }
 
-    ofstream MyFile("Problem_7.txt"); // "(o)fstream" writes to file
-    MyFile << A; //What we want in the file
-    MyFile.close(); //close the file
+    std::cout << A << std::endl;
+    
     return 0;
 }
--- Project_1/Code/Problem_7.py
+++ Project_1/Code/Problem_7.py
@@ -1,12 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x,u,v = np.loadtxt("Problem_7.txt", unpack=True) #numpy's txt-reading
-
+x,u = np.loadtxt("Problem_2.txt", unpack=True) #numpy's txt-reading
+x1, v1 = np.loadtxt("out10.txt", unpack=True)
+x2, v2 = np.loadtxt("out100.txt", unpack=True)
+x3, v3 = np.loadtxt("out1000.txt", unpack=True)
 # x,g,v = data[:,0], data[:,1], data[:,2] #putting x and y into arrays
 
 plt.plot(x,u,label="u") # plotting y over x
-plt.plot(x,v,label="v") 
+plt.plot(x1,v1,label="n=10")
+plt.plot(x2,v2,label="n=100")
+plt.plot(x3,v3,label="n=1000")
+# plt.plot(x,v,label="v") 
 #Plotting stuff:
 plt.xlabel("x")
 plt.ylabel("y")
