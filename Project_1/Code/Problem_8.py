834c2b974abe86acce1ccff41939bd14626471d4
Tensorboy2 <root@LAPTOP-RSSIV8M1.localdomain>
2022-09-08 14:11:31 +0200

    Ny problem_8

--- Project_1/Code/Problem_8.py
+++ Project_1/Code/Problem_8.py
@@ -5,26 +5,47 @@ x,u = np.loadtxt("Problem_2.txt", unpack=True) #numpy's txt-reading
 x1, v1 = np.loadtxt("out10.txt", unpack=True)
 x2, v2 = np.loadtxt("out100.txt", unpack=True)
 x3, v3 = np.loadtxt("out1000.txt", unpack=True)
+
 # Compute numpy arrays with the absolute error, relative error and log10(relative error)
-def error(d2u_approx, d2u_exact, name):
-    d2u_exact_1 = np.resize(d2u_exact,int(len(d2u_approx)))
-    abs_err = np.abs(d2u_approx - d2u_exact_1)
-    rel_err = np.abs(abs_err / d2u_exact_1)
+def error(d2u_approx, d2u_exact):
+    n = int((len(d2u_exact))/(len(d2u_approx)-1))
+    print(n)
+    d2u_exact_1 = np.append(d2u_exact[::n],d2u_exact[-1])
+    print(len(d2u_exact_1))
+    abs_err = np.absolute(d2u_approx - d2u_exact_1)
+    rel_err = np.absolute(abs_err / d2u_exact_1)
+    log10_abs_err = np.log10(abs_err)
     log10_rel_err = np.log10(rel_err)
 
-    x_1 = np.resize(x,int(len(d2u_approx)))
-    plt.plot(x_1, log10_rel_err, '--', c="0.8", linewidth=1.5)
-    plt.plot(x_1, log10_rel_err, '.', c="black", markersize=10)
-    # plt.title("Log10(relative error) " + common_title_string, fontsize=10)
-    plt.ylabel("log10(relative error)")
-    plt.xlabel("x")
-    plt.savefig(f"log10_rel_err_vs_x_{name}.pdf")
+    x_1 = np.linspace(0,1,len(d2u_exact_1))
     
+    return x_1, log10_abs_err, log10_rel_err
 
-error(v1,u,"n=10")
+#Def error arrays:
+x_1, log10_abs_err_10, log10_rel_err_10 = error(v1,u)
+x_2, log10_abs_err_100, log10_rel_err_100 = error(v2,u)
+x_3, log10_abs_err_1000, log10_rel_err_1000 = error(v3,u)
+
+#Plot abs erro:
+plt.figure(1)
+plt.plot(x_1, log10_abs_err_10,label="n=10")
+plt.plot(x_2, log10_abs_err_100,label="n=100")
+plt.plot(x_3, log10_abs_err_1000,label="n=1000")
+
+plt.xlabel("x")
+plt.ylabel("log(abs(v-u))")
+plt.legend
+plt.savefig("log10_abs_err.pdf")
+
+#Plot rel error:
+plt.figure(2)
+plt.plot(x_1, log10_rel_err_10,label="n=10")
+plt.plot(x_2, log10_rel_err_100,label="n=100")
+plt.plot(x_3, log10_rel_err_1000,label="n=1000")
+
+plt.xlabel("x")
+plt.ylabel("log(abs(v-u)/u))")
+plt.legend
+plt.savefig("log10_rel_err.pdf")
 
 
-error(v2,u,"n=100")
-
-
-error(v3,u,"n=1000")
