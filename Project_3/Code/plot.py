af13c65c1467491d77b67d677b8062b30a7f27f4
Nils T <n.e.c.taugbol@fys.uio.no>
2022-10-19 11:07:55 +0200

    plot file start

---
+++ Project_3/Code/plot.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+cm = 1 / 2.54
+plt.rcParams["figure.figsize"] = (12 * cm, 8 * cm)
+sns.set_theme("notebook", "whitegrid")
