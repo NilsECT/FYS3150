7efad80bc7ff6c944938b0d2ef8859d9fdaa8a72
Tensorboy2 <root@LAPTOP-RSSIV8M1.localdomain>
2022-10-19 13:08:25 +0200

    more in simulation generator

--- Project_3/Code/Simulation_generator.cpp
+++ Project_3/Code/Simulation_generator.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 //#include <string>
 
-void simulate(Penningtrap trap, bool has_coulomb_force,int N, double dt, std::string evolve) {
+void simulate(Penningtrap trap, bool has_coulomb_force,int N, double dt, std::string evolve, bool update_V) {
     std::string dt_str = std::to_string(dt);
     std::string has_col;
     if (has_coulomb_force) {
@@ -23,9 +23,11 @@ void simulate(Penningtrap trap, bool has_coulomb_force,int N, double dt, std::st
 
     for (int i = 0; i < N; i++) {
       trap.write_to_file(evolve,dt_str, has_col);
-      //trap.update_V_0(f,w,t); // Define updating parameters of the strength of the E-field.
+      /*if (update_V){
+        trap.update_V_0(f,w,t); // Define updating parameters of the strength of the E-field.
+      }*/
       if (evolve=="RK4"){
-      trap.evolve_RK4(dt, has_coulomb_force, true, true);
+        trap.evolve_RK4(dt, has_coulomb_force, true, true);
 
       std::cout << i << std::endl;
       }
@@ -56,8 +58,9 @@ int main(){
     Particle particle_1 = Particle(q,m,r,v);
     trap_1.add_particle(particle_1);
     bool has_coulomb_force = false;
+    bool update_V = false;
     std::string evolve = "RK4";
-    simulate(trap_1,has_coulomb_force,N,dt,evolve);
+    simulate(trap_1,has_coulomb_force,N,dt,evolve,update_V);
 
     // With Forward Euler:
     Penningtrap trap_2 = Penningtrap(B_0, V_0, d);
@@ -67,7 +70,7 @@ int main(){
     trap_2.add_particle(particle_2);
     has_coulomb_force = false;
     evolve = "Forward_Euler";
-    simulate(trap_2,has_coulomb_force,N,dt,evolve);
+    simulate(trap_2,has_coulomb_force,N,dt,evolve,update_V);
 
     // Two particles with interaction:
     // With RK4:
@@ -82,7 +85,7 @@ int main(){
     trap_3.add_particle(particle_4);
     has_coulomb_force = true;
     evolve = "RK4";
-    simulate(trap_3,has_coulomb_force,N,dt,evolve);
+    simulate(trap_3,has_coulomb_force,N,dt,evolve,update_V);
 
     // With Forward Euler:
     Penningtrap trap_4 = Penningtrap(B_0, V_0, d);
@@ -96,7 +99,7 @@ int main(){
     trap_4.add_particle(particle_6);
     has_coulomb_force = true;
     evolve = "Forward_Euler";
-    simulate(trap_4,has_coulomb_force,N,dt,evolve);
+    simulate(trap_4,has_coulomb_force,N,dt,evolve,update_V);
 
     // Two particles without interaction:
     // With RK4:
@@ -111,7 +114,7 @@ int main(){
     trap_5.add_particle(particle_8);
     has_coulomb_force = false;
     evolve = "RK4";
-    simulate(trap_5,has_coulomb_force,N,dt,evolve);
+    simulate(trap_5,has_coulomb_force,N,dt,evolve,update_V);
 
     // With Forward Euler:
     Penningtrap trap_6 = Penningtrap(B_0, V_0, d);
@@ -125,7 +128,47 @@ int main(){
     trap_6.add_particle(particle_10);
     has_coulomb_force = false;
     evolve = "Forward_Euler";
-    simulate(trap_6,has_coulomb_force,N,dt,evolve);
-    
+    simulate(trap_6,has_coulomb_force,N,dt,evolve,update_V);
+
+
+    //4 Different time steps:
+    std::vector<int> n = {4000,8000,16000,32000};
+    for (int k : n){
+      int N = k;
+      dt = 50./k;
+      trap_1 = Penningtrap(B_0, V_0, d);
+      r = arma::vec{20,0,20}; // micro m
+      v = arma::vec{0,25,0};
+      particle_1 = Particle(q,m,r,v);
+      trap_1.add_particle(particle_1);
+      bool has_coulomb_force = false;
+      bool update_V = false;
+      evolve = "RK";
+      simulate(trap_1,has_coulomb_force,k,dt,evolve,update_V);
+    }
+    for (int k : n){
+      int N = k;
+      dt = 50./k;
+      trap_1 = Penningtrap(B_0, V_0, d);
+      r = arma::vec{20,0,20}; // micro m
+      v = arma::vec{0,25,0};
+      particle_1 = Particle(q,m,r,v);
+      trap_1.add_particle(particle_1);
+      bool has_coulomb_force = false;
+      bool update_V = false;
+      evolve = "FE";
+      simulate(trap_1,has_coulomb_force,k,dt,evolve,update_V);
+    }
+
+
+    /*
+    // 100 particles with interaction:
+    Penningtrap trap_100 = Penningtrap(B_0, V_0, d);
+    trap_100.generate_particles(100,q,m,seed);
+    update_V = true;
+    has_coulomb_force = false;
+    evolve = ?;
+    simulate(trap_100,has_coulomb_force,N,dt,evolve,update_V);
+    */
     return 0;
 }
