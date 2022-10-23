fc412f299f7acc71f5083eab91693deebf69bde0
fridasor <f.o.sorensen@fys.uio.no>
2022-10-23 11:13:58 +0200

    Updates to perturbation-sim

--- Project_3/Code/Simulation_perturbation.cpp
+++ Project_3/Code/Simulation_perturbation.cpp
@@ -12,13 +12,13 @@ int main(){
   double m = 40.078;
   double d = 500;
 
-  int n_part = 25;
+  int seed = 137;
 
-  int T = 500;      // [microseconds]
-  double dt = 0.01; // [microseconds]
+  int n_part = 100;
+
+  double T = 500.0;      // [microseconds]
+  double dt = 0.013; // [microseconds]
   int N = T/dt;
-
-  bool has_coulomb_force = false;
 
   Penningtrap trap_100 = Penningtrap(B_0, V_0, d);
   trap_100.generate_particles(n_part, q, m, seed);
@@ -30,20 +30,40 @@ int main(){
   double w_max = 2.5; // [MHz]
   int M = w_max/dw;   // number of values for angular frequency w (ie. omega)
 
-  arma::vec f = {0.1, 0.4, 0.7};
+  arma::vec f = {0.1};//, 0.4, 0.7};
+
+  // Simulate with no particle interaction:
+  bool has_coulomb_force = false;
 
   for (double &amp : f) {
     std::cout << std::endl;
-    std::cout << "Simulate for " << N*dt << " microseconds, with amplitude f = " << amp <<"(with interaction)" <<  std::endl;
+    std::cout << "Simulate for " << N*dt << " microseconds, with amplitude f = " << amp <<"(without interaction)" <<  std::endl;
+    trap_100.reset_particles();
 
     for (double w = w_min; w <= w_max; w += dw) {
       std::cout << w << "    ";
-      trap_100.reset_particles();
       trap_100.simulate(has_coulomb_force, N, dt, "RK4", true, amp, w);
       trap_100.write_to_file_perturbation(amp, w, has_coulomb_force, n_part);
       std::cout << "Number of particles that left the trap: " << trap_100.num_particles_out << std::endl;
     }
   }
+
+  // Simulate with particle interaction:
+  has_coulomb_force = true;
+  /*
+  for (double &amp : f) {
+    std::cout << std::endl;
+    std::cout << "Simulate for " << N*dt << " microseconds, with amplitude f = " << amp <<"(with interaction)" <<  std::endl;
+    trap_100.reset_particles();
+
+    for (double w = w_min; w <= w_max; w += dw) {
+      std::cout << w << "    ";
+      trap_100.simulate(has_coulomb_force, N, dt, "RK4", true, amp, w);
+      trap_100.write_to_file_perturbation(amp, w, has_coulomb_force, n_part);
+      std::cout << "Number of particles that left the trap: " << trap_100.num_particles_out << std::endl;
+    }
+  }
+  */
 
   return 0;
 }
