5dd073c7db6816dd9c2db6f1de3314f646d88216
Tensorboy2 <root@LAPTOP-RSSIV8M1.localdomain>
2022-10-24 18:46:43 +0200

    hola nils

--- Project_3/Code/Simulation_perturbation.cpp
+++ Project_3/Code/Simulation_perturbation.cpp
@@ -14,7 +14,7 @@ int main(){
 
   int seed = 137;
 
-  int n_part = 100;
+  int n_part = 25;
 
   double T = 500.0;      // [microseconds]
   double dt = 0.01; // [microseconds]
@@ -25,15 +25,15 @@ int main(){
 
   std::string dt_str = std::to_string(dt);
   
-  double dw = 0.02;   // [MHz]
-  double w_min = 0.2; // [MHz]
-  double w_max = 2.5; // [MHz]
+  double dw = 0.005;   // [MHz]
+  double w_min = 1.6; // [MHz]
+  double w_max = 2.1; // [MHz]
 
-  arma::vec f = {0.1, 0.4, 0.7};
+  arma::vec f = {0.4};
 
   // Simulate with no particle interaction:
   bool has_coulomb_force = false;
-  /*
+  
   for (double &amp : f) {
     std::cout << std::endl;
     std::cout << "Simulate for " << N*dt << " microseconds, with amplitude f = " << amp <<"(without interaction)" <<  std::endl;
@@ -47,7 +47,7 @@ int main(){
       std::cout << "Number of particles that left the trap: " << trap_100.num_particles_out << std::endl;
     }
   }
-  */
+  
   // Simulate with particle interaction:
   has_coulomb_force = true;
   
@@ -58,11 +58,13 @@ int main(){
 
     for (double w = w_min; w < w_max+dw; w += dw) {
       std::cout << w << "    ";
+      trap_100.reset_particles();
       trap_100.simulate(has_coulomb_force, N, dt, "RK4", true, amp, w);
       trap_100.write_to_file_perturbation(amp, w, has_coulomb_force, n_part);
       std::cout << "Number of particles that left the trap: " << trap_100.num_particles_out << std::endl;
     }
   }
+  
   
 
   return 0;
