188ffe0b65421445cdeb16ce07b60de00ce0b6f0
Tensorboy2 <root@LAPTOP-RSSIV8M1.localdomain>
2022-11-16 16:00:11 +0100

    changed loops

--- Project_4/Code/main.cpp
+++ Project_4/Code/main.cpp
@@ -14,9 +14,9 @@ int main(){
   std::mt19937 MC_seed_generator (seed);
 
   // Problem 4: compare with analytical for L = 2
-  // std::vector<double> temperatures = {1.0, 2.0};
+  //std::vector<double> temperatures = {1.0, 2.0};
 
-  int N_MC_cycles = 1000000;
+  int N_MC_cycles = 1000;
   int N_spinflips = 1000;
 
   // model instance:
@@ -26,7 +26,7 @@ int main(){
   // model.analytical_comparison(temperatures, N_spinflips, N_MC_cycles);
 
 
-  // // Problem 5: study burn-in time
+  // Problem 5: study burn-in time
   arma::vec cycles = arma::logspace(1, 10, 50);
   // std::cout << "Starting ordered varying N for T=1.0" << std::endl;
   // model.varying_n_mc_cycles(1.0, cycles, "varying_cycles_ordered", MC_seed_generator(), true);
@@ -40,11 +40,11 @@ int main(){
   // std::cout << "Starting unordered varying N for T=2.4" << std::endl;
   // model.varying_n_mc_cycles(2.4, cycles, "varying_cycles_unordered", MC_seed_generator(), false);
 
-  arma::vec temperatures_p5 = arma::vec {1.0, 2.4};
-  model.varying_n_mc_cycles(temperatures_p5, cycles);
+  // arma::vec temperatures_p5 = arma::vec {1.0, 2.4};
+  // model.varying_n_mc_cycles(temperatures_p5, cycles);
 
-  // std::vector<double> temperatures = {1, 2.4};
-  // model.epsilon_dist(temperatures, 20, 100000, 10);
+  // std::vector<double> temperatures_2 = {1, 2.4};
+  // model.epsilon_dist(temperatures_2, 20, 100000, 10);
 
   // Problem 8:
   std::vector<int> lattice_sizes_p8 = {40, 60, 80, 100};
