4d6e6e4ee5d1018d39e1864c4393d89f3e8d3e54
Tensorboy2 <root@LAPTOP-RSSIV8M1.localdomain>
2022-11-16 19:03:06 +0100

    dummy parallel

--- Project_4/Code/main.cpp
+++ Project_4/Code/main.cpp
@@ -27,16 +27,31 @@ int main(){
 
 
   // // Problem 5: study burn-in time
-  arma::vec cycles = arma::logspace(1, 7, 50);
+  arma::vec cycles = arma::logspace(1, 6, 50);
   arma::vec temperatures_p5 = arma::vec {1., 1.5, 2., 2.4, 3.};
   model.varying_n_mc_cycles(temperatures_p5, cycles);
 
   std::vector<double> temperatures_2 = {1., 1.5, 2., 2.4, 3.};
   model.epsilon_dist(temperatures_2, 20, 100000, 10);
 
+  // // sigurd:
+  // int T_max = 2.4;
+  // int T_min = 2.3;
+
+
+  // // Nils:
+  // int T_max = 2.2;
+  // int T_min = 2.1;
+  
+
+  // // Brage:
+  // int T_max = 2.28;
+  // int T_min = 2.22;
+  
+
   // // Problem 8:
   std::vector<int> lattice_sizes_p8 = {40, 60, 80, 100};
-  arma::vec temperatures_p8 = arma::linspace(2.1, 2.4, 4);  // if we get results we need to upgrade this to more values
+  arma::vec temperatures_p8 = arma::linspace(T_min, T_max, 5);  // if we get results we need to upgrade this to more values
   model.phase_transitions(lattice_sizes_p8, temperatures_p8, N_spinflips, N_MC_cycles, seed);
 
   return 0;
