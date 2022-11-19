7f03948c6d2aa3ccb09fd4d153a2e740aec31d1b
Nils T <n.e.c.taugbol@fys.uio.no>
2022-11-19 16:04:11 +0100

    ready to drive

--- Project_4/Code/main.cpp
+++ Project_4/Code/main.cpp
@@ -15,58 +15,50 @@ int main(){
   // Problem 4: compare with analytical for L = 2
   //std::vector<double> temperatures = {1.0, 2.0};
 
-  int N_MC_cycles = 500000;
+  // int N_MC_cycles = 500000;
 
   // model instance:
   Ising model = Ising();
 
   // Working code for comparing with analytical solution (currently missing magnetization, will fix soon):
-  std::vector<double> temps = {1.0, 2.4};
-  model.analytical_comparison(temps, 10000000, "analytical_comparison", seed);
+  // std::vector<double> temps = {1.0, 2.4};
+  // model.analytical_comparison(temps, 10000000, "analytical_comparison", seed);
 
-  // --------
-  
-  // Code that currently does not work:
+  // trying to find burn in time based on cycles
+  arma::vec temperatures = arma::vec {1., 1.5, 2., 2.4};
 
-  // trying to find burn in time based on walks
-  arma::vec temperatures_burn = arma::vec {1., 1.5, 2., 2.4, 3.};
-  // std::vector<int> n_walks = {15000};
-  // int n_walks = 12000;
-  // std::vector<int> lattices = {20, 40};
-  // model.varying_n_walk(temperatures_burn, n_walks, lattices, 50);
+  // Sigurd spesial
+  // std::vector<int> lattices = std::vector<int> {20, 40, 60, 100};
+  // model.varying_n_mc_cycles(temperatures, 5000, 25, lattices);
+  // // looking for the distribution of the energies
+  // model.epsilon_dist(temperatures, lattices, 2000, 25);
 
-  // std::cout << "Starting analytical comparison" << std::endl;
-  // model.analytical_comparison(temperatures, N_spinflips, N_MC_cycles);
+  // Everyone else
+  // model.varying_n_mc_cycles(temperatures, 5000, 25, 20);
+  // looking for the distribution of the energies
+  // model.epsilon_dist(temperatures, 20, 2000, 25);
 
-  // SETT N=L^2 !!!!!!!!!!!!!!!!!!!!!!!
-
-  // // Problem 5: study burn-in time
-  arma::vec cycles = arma::logspace(1, 6, 50);
-  arma::vec temperatures_p5 = arma::vec {1., 1.5, 2., 2.4, 3.};
-  model.varying_n_mc_cycles(temperatures_p5, cycles);
-
-  std::vector<double> temperatures_2 = {1., 1.5, 2., 2.4, 3.};
-  model.epsilon_dist(temperatures_2, 20, 1000000, 10);
 
   // // sigurd:
-  // int T_max = 2.4;
-  // int T_min = 2.3;
+  // double T_max = 2.4;
+  // double T_min = 2.1;
 
 
   // // Nils:
-  int T_max = 2.2;
-  int T_min = 2.1;
+  // double T_max = 2.4;
+  // double T_min = 3.;
 
 
   // // Brage:
-  // int T_max = 2.28;
-  // int T_min = 2.22;
+  // double T_max = 1.;
+  // double T_min = 2.1;
 
 
-  // // // Problem 8:
-  std::vector<int> lattice_sizes_p8 = {40, 60, 80, 100};
-  arma::vec temperatures_p8 = arma::linspace(T_min, T_max, 5);  // if we get results we need to upgrade this to more values
-  model.phase_transitions(lattice_sizes_p8, temperatures_p8, N_spinflips, N_MC_cycles, seed);
+  // // Problem 8:
+  // std::vector<int> lattice_sizes_p8 = {40, 60, 80, 100};
+  std::vector<int> lattice_sizes_p8 = {20};
+  arma::vec temperatures_p8 = arma::linspace(T_min, T_max, 200);  // if we get results we need to upgrade this to more values
+  model.phase_transitions(lattice_sizes_p8, temperatures_p8, 1500, 50, seed, 1000);
 
   return 0;
 }
