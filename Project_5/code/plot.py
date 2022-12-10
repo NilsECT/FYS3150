70a07b56fe05dbc23746716940abde2744727fe5
fridasor <f.o.sorensen@fys.uio.no>
2022-12-10 13:09:30 +0100

    Updated plotcode

--- Project_5/code/plot.py
+++ Project_5/code/plot.py
@@ -11,20 +11,6 @@ plt.rcParams["mathtext.fontset"] = "dejavuserif"
 plt.rcParams['font.size'] = '16'
 
 sns.set_theme("notebook", "whitegrid")
-# sns.set(font_scale=2)
-# sns.set_style({'font.family':'serif', 'font.serif':'Times New Roman'})
-
-A = cube()
-A.load("Experiment_3.bin", arma_binary)
-print(size(A))
-P = np.array(A)
-
-u = cube()
-u.load('Experiment_3_Re.bin', arma_binary)
-u_real = np.array(u)
-
-u.load('Experiment_3_Im.bin', arma_binary)
-u_imag = np.array(u)
 
 # B = mat()
 # B.load("pot.bin", arma_binary)
@@ -77,15 +63,16 @@ fontsize = 15
 x_min, x_max = x_points[0], x_points[-1]
 y_min, y_max = y_points[0], y_points[-1]
 
+##### Plot colourmaps:
 
 time_points = [0, 0.001, 0.002]
+filenames = ['3', '3_Re', '3_Im']
 
-# Create figure
-# fig = plt.figure(figsize=(8, 7))
+for filename, label in zip(filenames, ['full', 'real', 'imag']):
 
-##### Plot colourmap:
-
-for probability, label in zip([P, u_real, u_imag], ['full', 'real', 'imag']):
+    A = cube()
+    A.load(f'Experiment_{filename}.bin', arma_binary)
+    probability = np.array(A)
 
     fig, axs = plt.subplots(1, 3, figsize=(16, 7))
     fontsize = 15
@@ -110,7 +97,7 @@ for probability, label in zip([P, u_real, u_imag], ['full', 'real', 'imag']):
         axs[i].set_ylabel("$y$", fontsize=fontsize)
 
 
-        axs[i].title.set_text(f'$t = {t}$', fontsize=fontsize)
+        axs[i].title.set_text(f'$t = {t}$')
 
     plt.tight_layout()
     # Make space for title
@@ -118,137 +105,8 @@ for probability, label in zip([P, u_real, u_imag], ['full', 'real', 'imag']):
 
     plt.savefig(f'figs/colourmap_{label}.pdf')
 
-##### Plot real part:
-
-# fig, axs = plt.subplots(1, 3, figsize=(16, 6))
-# fontsize = 11
-#
-# for i, t in enumerate(time_points):
-#
-#     t_idx = int(t / dt)
-#
-#     # axs[i] = plt.gca()
-#     axs[i].grid(False)
-#
-#     norm = mpl.cm.colors.Normalize(vmin=0.0, vmax=np.max(u_real[t_idx, :, :]))
-#     img = axs[i].imshow(u_real[t_idx, :, :], extent=[x_min,x_max,y_min,y_max], cmap=plt.get_cmap("hot"))#, norm=norm)
-#     img.set_norm(norm)
-#
-#     cbar = fig.colorbar(img, ax=axs[i], fraction = 0.05)
-#
-#     cbar.set_label(f'$p(x,y; t)$', fontsize=fontsize)
-#     cbar.ax.tick_params(labelsize=fontsize)
-#
-#     axs[i].set_xlabel("$x$", fontsize=fontsize)
-#     axs[i].set_ylabel("$y$", fontsize=fontsize)
-#
-#     axs[i].title.set_text(f'$t = {t}$')
-#
-# plt.tight_layout()
-# # Make space for title
-# plt.subplots_adjust(top=0.85)
-#
-# plt.savefig(f'figs/colourmap_real.pdf')
-
-##### Plot imaginary part:
-
-
-# quit()
-#
-# for t in time_points:
-#
-#     ### Plot colourmap of probability:
-#
-#     t_idx = int(t / dt)
-#
-#     # Create figure
-#     fig = plt.figure(figsize=(8, 7))
-#
-#     ax = plt.gca()
-#     ax.grid(False)
-#
-#     norm = mpl.cm.colors.Normalize(vmin=0.0, vmax=np.max(P[t_idx, :, :]))
-#
-#     # Plot the first frame
-#     img = ax.imshow(P[t_idx, :, :], extent=[x_min,x_max,y_min,y_max], cmap=plt.get_cmap("hot"), norm=norm)
-#     img.set_norm(norm)
-#     # Axis labels
-#     plt.xlabel("$x$", fontsize=fontsize)
-#     plt.ylabel("$y$", fontsize=fontsize)
-#     plt.xticks(fontsize=fontsize)
-#     plt.yticks(fontsize=fontsize)
-#
-#     # Add a colourbar
-#     cbar = fig.colorbar(img, ax=ax)
-#     cbar.set_label(f"$p(x,y; t = ${t})", fontsize=fontsize)
-#     cbar.ax.tick_params(labelsize=fontsize)
-#     plt.savefig(f'figs/colourmap_{t}.pdf')
-#
-#     ### Plot real part:
-#
-#     # Create figure
-#     fig = plt.figure(figsize=(8, 7))
-#
-#     ax = plt.gca()
-#     ax.grid(False)
-#
-#     norm = mpl.cm.colors.Normalize(vmin=0.0, vmax=np.max(u_real[t_idx, :, :]))
-#
-#     # Plot the first frame
-#     img = ax.imshow(u_real[t_idx, :, :], extent=[x_min,x_max,y_min,y_max], cmap=plt.get_cmap("hot"), norm=norm)
-#     img.set_norm(norm)
-#     # Axis labels
-#     plt.xlabel("$x$", fontsize=fontsize)
-#     plt.ylabel("$y$", fontsize=fontsize)
-#     plt.xticks(fontsize=fontsize)
-#     plt.yticks(fontsize=fontsize)
-#
-#     # Add a colourbar
-#     cbar = fig.colorbar(img, ax=ax)
-#     cbar.set_label(f"Real part of $u(x,y; t = {t})$", fontsize=fontsize)
-#     cbar.ax.tick_params(labelsize=fontsize)
-#     plt.savefig(f'figs/colourmap_real_{t}.pdf')
-#
-#     ### Plot imaginary part:
-#
-#     # Create figure
-#     fig = plt.figure(figsize=(8, 7))
-#
-#     ax = plt.gca()
-#     ax.grid(False)
-#
-#     norm = mpl.cm.colors.Normalize(vmin=0.0, vmax=np.max(u_imag[t_idx, :, :]))
-#
-#     # Plot the first frame
-#     img = ax.imshow(u_imag[t_idx, :, :], extent=[x_min,x_max,y_min,y_max], cmap=plt.get_cmap("hot"), norm=norm)
-#     img.set_norm(norm)
-#     # Axis labels
-#     plt.xlabel("$x$", fontsize=fontsize)
-#     plt.ylabel("$y$", fontsize=fontsize)
-#     plt.xticks(fontsize=fontsize)
-#     plt.yticks(fontsize=fontsize)
-#
-#     # Add a colourbar
-#     cbar = fig.colorbar(img, ax=ax)
-#     cbar.set_label(f"Imaginary part of $u(x,y; t = {t})$", fontsize=fontsize)
-#     cbar.ax.tick_params(labelsize=fontsize)
-#     plt.savefig(f'figs/colourmap_imag_{t}.pdf')
-
 
 ########### Plot "detector screen" for different slit numbers:
-
-# def load_detection_experiment(number):
-#     A = cube()
-#     A.load(f"Experiment_{number}.bin", arma_binary)
-#     P_detection = np.array(A)
-#
-#     # Find indices corresponding to t = 0.002, x = 0.8
-#     x_coord = 0.8
-#     t = 0.002
-#     t_idx = -1
-#     x_idx = int(x_coord / h)
-#
-#     return P_detection[t_idx, x_idx, :]/max(P_detection[t_idx, x_idx, :])
 
 A = cube()
 A.load("Experiment_3.bin", arma_binary)
@@ -263,7 +121,8 @@ P_triple = np.array(A)
 # Find indices corresponding to t = 0.002, x = 0.8
 x_coord = 0.8
 t = 0.002
-t_idx = -1
+t_idx = int(t / dt)
+print(t_idx)
 x_idx = int(x_coord / h)
 
 # Set plotting parameters
@@ -273,15 +132,11 @@ sns.set_palette(palette=palette, desat=0.9, color_codes=True)
 # Create figure
 plt.figure(figsize=(12, 7))
 
-x_points = np.linspace(y_min, y_max, len(P[t_idx, x_idx, :]))
+x_points = np.linspace(y_min, y_max, len(P_double[t_idx, x_idx, :]))
 
-# sns.lineplot(y=load_detection_experiment(3), x=x_points, label='Two slits')
-# sns.lineplot(y=load_detection_experiment(4), x=x_points, label='One slit')
-# sns.lineplot(y=load_detection_experiment(5), x=x_points, label='Three slits')
-
-sns.lineplot(y=P_double[t_idx, x_idx, :] / np.max(P_double[t_idx, x_idx, :]), x=x_points, label='Two slits')
-sns.lineplot(y=P_single[t_idx, x_idx, :] / np.max(P_single[t_idx, x_idx, :]), x=x_points, label='One slit')
-sns.lineplot(y=P_triple[t_idx, x_idx, :] / np.max(P_triple[t_idx, x_idx, :]), x=x_points, label='Three slits')
+sns.lineplot(y=P_double[t_idx, x_idx, :] / np.sum(P_double[t_idx, x_idx, :]), x=x_points, label='Two slits')
+sns.lineplot(y=P_single[t_idx, x_idx, :] / np.sum(P_single[t_idx, x_idx, :]), x=x_points, label='One slit')
+sns.lineplot(y=P_triple[t_idx, x_idx, :] / np.sum(P_triple[t_idx, x_idx, :]), x=x_points, label='Three slits')
 
 fontsize = 22
 plt.xlabel('$y$', fontsize=fontsize)
